6bad68ed5ea3bf284160fd8097fc45e6daab7316
BvspSite <bvsppress@gmail.com>
2025-11-01 08:47:19 +0700

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Fabrilio/', // Tambahkan ini sesuai dengan nama repository
+  base: './', // Ganti dari '/Fabrilio/' ke './'
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
